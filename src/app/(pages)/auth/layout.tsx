bdfc3d429a1a9aa5bdba0d76a644b47f89ea6d62
richard adebayo <richardebrain20@gmail.com>
2025-03-25 15:55:49 +0100

    feat: add OrgPage component and update styles in login and auth layout pages

--- src/app/(pages)/auth/layout.tsx
+++ src/app/(pages)/auth/layout.tsx
@@ -1,6 +1,6 @@
 import AuthProvider from "@/contexts/AuthProvider";
 import React from "react";
-import {ToastContainer} from 'react-toastify'
+import { ToastContainer } from "react-toastify";
 import Image from "next/image";
 
 // didn't export this layout
@@ -9,17 +9,17 @@ export default function AuthLayout({
 }: {
   children: React.ReactNode;
 }) {
-
   return (
-    <main className="w-full relative min-h-[100vh] grid place-items-center p-5 md:p-8 [background:radial-gradient(#0085FF,#003465)]">
-      <div className="auth-container w-full relative px-5 md:px-8 py-8 flex flex-col place-items-center">
+    <main className="w-full min-h-[100vh] grid place-items-center p-5 md:p-8 relative z-[100] [background:radial-gradient(#0085FF,#003465)]">
+      <div className="auth-container w-full relative bg- px-5 md:px-8 py-8 flex flex-col place-items-center">
         <div className=" hidden md:inline-block">
           <Image
             src={"/assets/auth/icon1.png"}
             alt="icon1"
             width={400}
             height={400}
-            className="fixed bottom-[0px] left-[300px] opacity-25 "
+            className="fixed bottom-[0px] left-[300px] opacity-5 bg-blend-luminosity -z-10 "
+            style={{ opacity: '50%' }}
           ></Image>
 
           <Image
